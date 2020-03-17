f127e2a25b2bb6f922e21a80c2db2515e83a5742
Supriya <kiranbarikjd@gmail.com>
2020-03-18 01:23:54 +0530

    Updates

---
+++ precache-manifest.82cdf1c86fc04ffc950355f77daaec93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733241298f4f5cb020535af36f0c8df3",
+    "url": "/corona-app/index.html"
+  },
+  {
+    "revision": "56f12e62584f267664dd",
+    "url": "/corona-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "02f37c11306651c760f0",
+    "url": "/corona-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56f12e62584f267664dd",
+    "url": "/corona-app/static/js/2.9e57c1d8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/corona-app/static/js/2.9e57c1d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f37c11306651c760f0",
+    "url": "/corona-app/static/js/main.fa022e13.chunk.js"
+  },
+  {
+    "revision": "9015aaf8067ea54a05d2",
+    "url": "/corona-app/static/js/runtime-main.4e720ffe.js"
+  }
+]);
